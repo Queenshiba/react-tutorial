c4c516c9ce897afa08c31a1de2d9deba96a26168
Queenshiba <haruka0111.2a@gmail.com>
2021-01-13 16:34:22 +0900

    declaring a winner

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Game from './Game'
+import Board from './Board'
 import './index.css';
 // import App from './App';
 // import reportWebVitals from './reportWebVitals';
@@ -11,6 +12,7 @@ ReactDOM.render(
 
   document.getElementById('root')
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
